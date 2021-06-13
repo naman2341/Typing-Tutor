7c0ffd8c21e937029b924f246036933cf7b30c13
pandenaman007 <62431605+pandenaman007@users.noreply.github.com>
2021-06-13 20:38:55 +0530

    Delete typingtutorial.h
    delete

--- typingtutorial.h
+++
@@ -1,8 +0,0 @@
-#define FILEREADSIZE 5000
-void createcontent();
-void inittyping();
-void createbuffer(char * inptr);
-void processstatistics();
-void matchwordinbuffer(char * token);
-void readfile();
-int istokeninbuffer(char *inptr,int bufferlength);
